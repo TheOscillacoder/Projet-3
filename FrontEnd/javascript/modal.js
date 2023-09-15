6783cb89ced84d9a2dfbd5bd678b59e2f192715a
TheOscillacoder <guillaume.caro92@gmail.com>
2023-09-15 13:04:18 +0200

    ajout des dossiers JS et HTML et leurs fichiers

---
+++ FrontEnd/javascript/modal.js
@@ -0,0 +1,86 @@
+// faire un appel a l'api pour générer les projets comme dans projets.js
+// ajouter en js le logo poubelle pour supprimer un projet
+//ajouter un add event listener sur le logo qui fait un apel a l'api pour supprimer le projet correspondant
+
+
+let modal = null
+const focusableSelector = "button, a, input, textarea"
+let focusables = []
+let previouslyFocusedElement = null
+
+
+
+const openModal = function (event) {
+    event.preventDefault()
+    modal = document.querySelector(event.target.getAttribute("href"))
+    
+    // focusables = Array.from(modal.querySelectorAll(focusableSelector))
+    // previouslyFocusedElement = document.querySelector(":focus") 
+    // focusables[0].focus()
+    // cible l'attribut style="display" écris précédemment dans le fichier index.html
+    modal.style.display = null
+
+     // cible l'attribut arial-hidden écris précédemment dans le fichier index.html
+    modal.removeAttribute("aria-hidden")
+    modal.setAttribute("aria-modal", "true")
+    modal.addEventListener("click", closeModal)
+    modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
+    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+}
+
+const closeModal = function(event) {
+    if (modal === null) return
+    // if (previouslyFocusedElement !== null) previouslyFocusedElement.focus()
+    event.preventDefault()
+    // window.setTimeout(function () {
+    //     modal.style.display = "none"
+    // }, 500)
+
+    modal.style.display = "none"
+    modal.setAttribute("aria-hidden", true)
+    modal.removeAttribute("aria-modal")
+    modal.removeEventListener("click", closeModal)
+    modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
+    modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)    
+}
+
+const stopPropagation = function (event) {
+    event.stopPropagation()
+}
+
+// const focusInModal = function (event) {
+//     event.preventDefault()
+    
+//     let index = focusables.findIndex(f => f === modal.querySelector(":focus"))
+//     if (event.shiftkey === true) {
+//         index--
+//     } else {
+//         index++
+//     }
+    
+//     if (index >= focusables.length) {
+//         index = 0
+//     }
+//     if (index < 0) {
+//         index = focusables.length - 1
+//     }
+
+//     focusables[index].focus()
+// }
+
+// document.querySelectorAll(".modal-js").forEach(a => {
+//     a.addEventListener("click", openModal)   
+// })
+
+
+ window.addEventListener("keydown", function (event) {
+     if (event.key === "Escape" || event.key === "Esc") {
+         closeModal(event)
+     }
+     if(event.key === "tab" && modal !== null){
+         focusInModal(event)
+     }
+ })
+
+
+document.querySelector(".modal").addEventListener("click", openModal)
--- FrontEnd/javascript/projets.js
+++ FrontEnd/javascript/projets.js
@@ -1,13 +1,30 @@
       
-          async function genererProjets() {
+          async function genererProjets(IdCategory = 0) {
              const reponse = await fetch("http://localhost:5678/api/works");
               const projets = await reponse.json();
         
-              const sectionProjets = document.querySelector(".gallery");  
+              const sectionProjets = document.querySelector(".gallery");
+            //   if (IdCategory != 0) {
+
+                nettoyage(sectionProjets);
+            //   }  
 
                  for (let i = 0; i < projets.length; i++) {
-        
-                    const figureElement = document.createElement("figure");
+
+        const figureElement = document.createElement("figure");
+                    figureElement.setAttribute("id", projets[i].categoryId);
+
+                    if (IdCategory != 0) {
+                        
+                       if (IdCategory != projets[i].categoryId) {
+                        figureElement.style.display = "none";
+                        
+                    }   else {
+                        figureElement.style.display = "inherit";
+                    }
+                }
+
+// création des projets de la galerie
                     const imageElement = document.createElement("img");        
                         imageElement.setAttribute("src", projets[i].imageUrl);
                         imageElement.setAttribute("alt", projets[i].title);     
@@ -16,11 +33,65 @@
                         nomElement.textContent = projets[i].title;
         
                     figureElement.append(imageElement);
-                    figureElement.append(nomElement);        
-                    sectionProjets.append(figureElement);               
+                    figureElement.append(nomElement);  
+                    sectionProjets.append(figureElement); 
                   }
                   
                   console.log(projets); 
               }
 
 genererProjets();
+
+
+ async function categorieFiltre () {
+     const reponse = await fetch ("http://localhost:5678/api/categories");
+    let projetCategorie = await reponse.json();
+
+        const filterGroup = document.querySelector(".category-filters");
+        const ProjetGroup = document.querySelector(".gallery");
+
+
+        // Ajout du bouton pour toutes les catégories de travaux
+      
+        
+       const filterSoloAll = document.createElement("button");
+       filterSoloAll.innerText = "Tous";
+
+       filterGroup.insertBefore(filterSoloAll, ProjetGroup);
+
+       filterSoloAll.addEventListener(
+        "click", function () {
+            genererProjets(0)
+        }
+        )
+            
+
+     
+        // générer un bouton par catégorie avec leur nom respectif
+
+         for (let i = 0; i < projetCategorie.length; i++) {
+      const filterSolo = document.createElement("button");
+      filterSolo.innerText = projetCategorie[i].name;
+    
+
+      filterGroup.insertBefore(filterSolo, ProjetGroup);
+
+      filterSolo.addEventListener(
+        "click", function () {
+            genererProjets(projetCategorie[i].id);
+        }
+      )
+      }
+    
+         }        
+
+     categorieFiltre();
+
+
+
+
+function nettoyage(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
