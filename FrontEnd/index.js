f07d79158256bdaf76b290fe80f2ee8bb4ceab3a
TheOscillacoder <guillaume.caro92@gmail.com>
2023-10-03 17:23:08 +0200

    nouvelle arborescence + login response ok

---
+++ FrontEnd/index.js
@@ -0,0 +1,105 @@
+      
+          async function genererProjets(IdCategory = 0) {
+             const reponse = await fetch("http://localhost:5678/api/works");
+              const projets = await reponse.json();
+        
+              const sectionProjets = document.querySelector(".gallery");
+            //   if (IdCategory != 0) {
+
+                nettoyage(sectionProjets);
+            //   }  
+
+                 for (let i = 0; i < projets.length; i++) {
+
+        const figureElement = document.createElement("figure");
+                    figureElement.setAttribute("id", projets[i].categoryId);
+
+                    if (IdCategory != 0) {
+                        
+                       if (IdCategory != projets[i].categoryId) {
+                        figureElement.style.display = "none";
+                        
+                    }   else {
+                        figureElement.style.display = "inherit";
+                    }
+                }
+
+// création des projets de la galerie
+                    const imageElement = document.createElement("img");        
+                        imageElement.setAttribute("src", projets[i].imageUrl);
+                        imageElement.setAttribute("alt", projets[i].title);     
+            
+                    const nomElement = document.createElement("figcaption");
+                        nomElement.textContent = projets[i].title;
+        
+                    figureElement.append(imageElement);
+                    figureElement.append(nomElement);  
+                    sectionProjets.append(figureElement); 
+                  }
+                  
+                  console.log(projets); 
+              }
+
+genererProjets();
+
+
+ async function categorieFiltre () {
+     const reponse = await fetch ("http://localhost:5678/api/categories");
+    let projetCategorie = await reponse.json();
+//ajouter une div qui contient uniquement les boutons 
+        
+        const filterGroup = document.querySelector(".category-filters");
+        const filterDiv = document.querySelector(".filters");
+        const ProjetGroup = document.querySelector(".gallery");
+
+
+        // Ajout du bouton pour toutes les catégories de travaux
+      
+        
+       const filterSoloAll = document.createElement("button");
+       filterSoloAll.innerText = "Tous";
+
+       filterGroup.appendChild(filterSoloAll);
+    // filterGroup.insertBefore(filterSoloAll, filterDiv);
+        
+
+       filterSoloAll.addEventListener(
+        "click", function () {
+            genererProjets(0)
+        }
+        )
+            
+
+     
+        // générer un bouton par catégorie avec leur nom respectif
+
+         for (let i = 0; i < projetCategorie.length; i++) {
+      const filterSolo = document.createElement("button");
+      filterSolo.innerText = projetCategorie[i].name;
+    
+
+    // filterGroup.insertBefore(filterSolo, ProjetGroup);
+//    filterGroup.insertBefore(filterSolo, filterDiv);
+   
+   
+   filterGroup.appendChild(filterSolo);
+
+      filterSolo.addEventListener(
+        "click", function () {
+            genererProjets(projetCategorie[i].id);
+        }
+      )
+      }
+    
+         }        
+
+     categorieFiltre();
+
+
+
+
+function nettoyage(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
---
+++ FrontEnd/login.js
@@ -0,0 +1,90 @@
+
+
+
+// const editionDiv = document.querySelector(".edition-mode");
+// const changementsBaliseP = document.querySelector(".changement-edition");
+// const navClassEdition = document.querySelector(".nav-index");
+
+async function loginSubmit(event) {
+    event.preventDefault();
+
+
+    // Variables qui contiennent les valeurs rentrées dans le formulaire de connexion de la page.
+const userEmail = document.getElementById("email").value;
+const userPassword = document.getElementById("password").value;
+
+  // Envoi par fetch vers l'API des valeurs rentrées dans le formulaire
+fetch("http://localhost:5678/api/users/login", {
+
+method: "POST",
+
+headers: { "Content-Type": "application/json"
+
+},
+
+body: JSON.stringify({
+
+    email: userEmail,
+
+    password: userPassword,
+
+    })
+})
+
+//Traitement de la réponse et comportement en cas de champs vide/ erreur dans les données entrées
+.then ((response) => {
+    /////////// si données entrées sont correctes : ///////
+    if (response.ok) {
+
+    // Ajouter le display des classes en mode edition
+
+    // editionDiv.classList.(".edition-mode");
+    // changementsBaliseP.classList.(".changement-text");
+    // navClassEdition.classList.(".nav-index");
+    
+    console.log(response);
+    return response.json();
+    
+
+////////// Comportement en cas de champs vide : /////////
+    } else if (response.status !== 200 && (!userEmail || !userPassword)) {
+        document.querySelector(".error-message").innerHTML = "Saisissez un identifiant et un mot de passe."
+
+////////// Comportement en cas d'erreur dans les données entrées dans les 2 champs de saisie //////
+    } else if (response.status !== 200 && userEmail && userPassword) {
+        console.log("error");
+
+        document.querySelector(".error-message").innerHTML = " ! Erreur dans l'identifiant ou le mot de passe !"
+     }
+})
+.then ((userInfo) => {
+
+    let getToken = window.localStorage.getItem("sessionUserInfo");
+    
+    if (getToken === null) {
+        sessionStorage.setItem("sessionUserInfo", userInfo.token);
+
+    } else (window.location.href="index.html")
+})
+
+}
+// exécution de la fonction "loginSubmit" au champs "input type=submit" du formulaire de login
+const formLogin = document.querySelector(".login-form");
+formLogin.addEventListener("submit", loginSubmit);
+
+
+// Retrait du Token, du mode édition, et du "Logout" pour retour à "Login" dans le nav"
+const logoutToken = document.getElementById("logout");
+    logoutToken.addEventListener("click", function (event) {
+
+        event.preventDefault();
+        window.sessionStorage.removeItem("sessionUserInfo");
+
+        // editionDiv.classList.remove(".edition-mode");
+        // changementsBaliseP.classList.remove(".changement-text");
+        // navClassEdition.classList.remove(".nav-index")
+    });
+
+
+//email: sophie.bluel@test.tld
+//password: S0phie 
